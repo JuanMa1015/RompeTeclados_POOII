44f16c6235032adcedb4a5b2d475bf97b7853c73
Juan Manuel <gonzalesjuanmanuel645@gmail.com>
2025-04-06 12:51:59 -0500

    Feat: Cambio de la bd. Mejoras en las validaciones de registrar. Cambio de tiempo de sesion. Cambio de plantillas de envío de correos.

--- RompeTec/flask_auth_project/src/config.py
+++ RompeTec/flask_auth_project/src/config.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 class Config:
     SECRET_KEY = 'RompeRompe'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://Login_owner:npg_Ko6On9iyYSZp@ep-tight-haze-a5hccwic-pooler.us-east-2.aws.neon.tech/Login?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://Login_owner:npg_g8UeaTVX2fmz@ep-misty-butterfly-a56n04dp-pooler.us-east-2.aws.neon.tech/Login?sslmode=require'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -10,4 +10,4 @@ class Config:
     
     MAIL_DEFAULT_SENDER = "gonzalezjuanmanuel645@gmail.com"
     
-    PERMANENT_SESSION_LIFETIME = timedelta(minutes=1)
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=5)
